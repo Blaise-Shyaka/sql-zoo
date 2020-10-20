372fe5f3e18aae66b911700521baca0e2e241150
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-10-21 00:30:20 +0200

    add queries for zoo-select-from-world tutorial

--- zoo.sql
+++ zoo.sql
@@ -54,3 +54,36 @@ SELECT capital, name FROM world WHERE capital LIKE concat('%', name, '%');
 SELECT capital, name FROM world WHERE capital LIKE concat(name, '%_%');
 
 SELECT name, REPLACE(capital, name, '') FROM world WHERE capital LIKE concat(name, '%_%');
+
+/* SELECT from WORLD Tutorial */
+
+SELECT name FROM world
+WHERE population >= 200000000;
+
+SELECT name, (gdp/population) AS 'GDP/Capita' FROM world WHERE population >= 200000000;
+
+SELECT name, (population/1000000) FROM world WHERE continent = 'South America';
+
+SELECT name, population FROM world WHERE name IN ('France', 'Germany', 'Italy');
+
+SELECT name FROM world WHERE name LIKE '%United%';
+
+SELECT name, population, area FROM world WHERE area > 3000000 OR population > 250000000;
+
+SELECT name, population, area FROM world WHERE area > 3000000 XOR population > 250000000;
+
+SELECT name, ROUND(population/1000000, 2), ROUND(gdp/1000000000, 2) FROM world WHERE continent = 'South America';
+
+SELECT name, ROUND(gdp/population, -3) FROM world WHERE gdp > 1000000000000;
+
+SELECT name, capital FROM world
+ WHERE LENGTH(name) = LENGTH(capital);
+
+SELECT name, capital
+FROM world WHERE LEFT(name, 1) = LEFT(capital, 1) AND name <> capital;
+
+SELECT name
+   FROM world
+WHERE name LIKE '%a%' AND name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%' AND name LIKE '%e%'
+  AND name NOT LIKE '% %';
+
