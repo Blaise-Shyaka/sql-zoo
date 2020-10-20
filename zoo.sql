ec3ff8648532d8db175755d357db60a415b4dc31
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-10-20 20:32:15 +0200

    add queries for zoo-select-names tutorial

--- zoo.sql
+++ zoo.sql
@@ -8,3 +8,49 @@ SELECT name, population FROM world
 SELECT name, area FROM world
   WHERE area BETWEEN 200000 AND 250000;
 
+/* SELECT names */
+
+SELECT name FROM world
+  WHERE name LIKE 'Y%';
+
+SELECT name FROM world
+  WHERE name LIKE '%Y';
+
+SELECT name FROM world
+  WHERE name LIKE '%x%';
+
+SELECT name FROM world
+  WHERE name LIKE '%land';
+
+SELECT name FROM world
+  WHERE name LIKE 'C%ia';
+
+SELECT name FROM world
+  WHERE name LIKE '%oo%';
+
+SELECT name FROM world
+  WHERE name LIKE '%a%a%a%';
+
+SELECT name FROM world
+ WHERE name LIKE '_t%'
+ORDER BY name;
+
+SELECT name FROM world
+ WHERE name LIKE '%o__o%';
+
+SELECT name FROM world
+ WHERE name LIKE '____';
+
+SELECT name
+  FROM world
+ WHERE name LIKE capital;
+
+SELECT name
+  FROM world
+ WHERE capital LIKE concat(name, ' ', 'City');
+
+SELECT capital, name FROM world WHERE capital LIKE concat('%', name, '%');
+
+SELECT capital, name FROM world WHERE capital LIKE concat(name, '%_%');
+
+SELECT name, REPLACE(capital, name, '') FROM world WHERE capital LIKE concat(name, '%_%');
