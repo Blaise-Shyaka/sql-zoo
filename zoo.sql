d7aa0f99b7321d26a921cd4e6e5ad248e019ef95
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-10-21 15:39:33 +0200

    add queries for zoo-nobel-tutorial

--- zoo.sql
+++ zoo.sql
@@ -87,3 +87,41 @@ SELECT name
 WHERE name LIKE '%a%' AND name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%' AND name LIKE '%e%'
   AND name NOT LIKE '% %';
 
+/* SELECT from Nobel Tutorial */
+
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950;
+
+SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature';
+
+SELECT yr, subject FROM nobel WHERE winner = 'Albert Einstein';
+
+SELECT winner FROM nobel WHERE subject = 'Peace' AND yr >= 2000;
+
+SELECT yr, subject, winner FROM nobel WHERE subject = 'Literature' AND yr >= 1980 AND yr < 1990;
+
+SELECT * FROM nobel
+ WHERE winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter', 'Barack Obama');
+
+SELECT winner FROM nobel WHERE winner LIKE 'John%';
+
+SELECT yr, subject, winner FROM nobel WHERE subject = 'Physics' AND yr = 1980 OR subject = 'Chemistry' AND yr = 1984;
+
+SELECT yr, subject, winner FROM nobel WHERE yr = 1980 AND subject NOT IN ('Chemistry', 'Medicine');
+
+SELECT yr, subject, winner FROM nobel WHERE subject = 'Medicine' AND yr <= 1909 OR subject = 'Literature' AND yr >= 2004;
+
+SELECT * FROM nobel WHERE winner = 'PETER GRÜNBERG';
+
+SELECT * FROM nobel WHERE winner = 'EUGENE O''NEILL';
+
+SELECT winner, yr, subject FROM nobel WHERE winner LIKE 'Sir%' ORDER BY yr DESC, winner;
+
+SELECT winner, subject
+  FROM nobel
+ WHERE yr=1984
+ ORDER BY CASE WHEN subject IN ('Physics', 'Chemistry') THEN 1 ELSE 0 END, subject,winner;
