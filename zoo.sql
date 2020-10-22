8ff460d0f6c91ac46376e00acc64fa2bed183f24
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-10-22 02:21:23 +0200

    add queries for zoo-sum-and-count tutorial

--- zoo.sql
+++ zoo.sql
@@ -144,3 +144,22 @@ SELECT continent, name, area FROM world x
         WHERE y.continent=x.continent);
 
 SELECT continent, name FROM world x WHERE name = (SELECT name FROM world y WHERE x.continent = y.continent ORDER BY name LIMIT 1);
+
+/* SUM and COUNT */
+
+SELECT SUM(population)
+FROM world;
+
+SELECT DISTINCT continent FROM world;
+
+SELECT SUM(gdp) FROM world WHERE continent = 'Africa';
+
+SELECT COUNT(name) FROM world WHERE area >= 1000000;
+
+SELECT SUM(population) FROM world WHERE name IN ('Estonia', 'Latvia', 'Lithuania');
+
+SELECT continent, COUNT(name) FROM world GROUP BY(continent);
+
+SELECT continent, COUNT(name) FROM world WHERE population >= 10000000 GROUP BY continent;
+
+SELECT continent FROM world GROUP BY continent HAVING SUM(population) >= 100000000;
