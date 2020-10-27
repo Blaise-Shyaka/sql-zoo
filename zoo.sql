216b10b2a98bf8deeda3a5c920ade26ef04e90d0
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-10-27 18:13:36 +0200

    Add queries for zoo-window-functions

--- zoo.sql
+++ zoo.sql
@@ -288,3 +288,33 @@ SELECT subject, SUM(response * A_STRONGLY_AGREE/100)
  WHERE question='Q22'
    AND subject IN ('(8) Computer Science', '(H) Creative Arts and Design') GROUP BY subject;
 
+/* Window functions */
+
+SELECT lastName, party, votes
+  FROM ge
+ WHERE constituency = 'S14000024' AND yr = 2017
+ORDER BY votes DESC;
+
+SELECT party, votes,
+       RANK() OVER (ORDER BY votes DESC) as posn
+  FROM ge
+ WHERE constituency = 'S14000024' AND yr = 2017
+ORDER BY party;
+
+SELECT yr,party, votes,
+      RANK() OVER (PARTITION BY yr ORDER BY votes DESC) as posn
+  FROM ge
+ WHERE constituency = 'S14000021'
+ORDER BY party,yr;
+
+SELECT constituency, party
+FROM ge x 
+WHERE constituency BETWEEN 'S14000021' AND 'S14000026'
+AND yr  = 2017 AND votes >= ALL(SELECT votes FROM ge y  WHERE x.constituency = y. constituency AND y.yr = 2017)
+ORDER BY constituency,votes DESC;
+
+SELECT party , COUNT(*)
+FROM ge x 
+WHERE constituency like 'S%'
+AND yr  = 2017 AND votes >= ALL(SELECT votes FROM ge y  WHERE x.constituency = y. constituency AND y.yr = 2017)
+GROUP BY party;
